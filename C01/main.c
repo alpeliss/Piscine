68ec35163442964c25edfea84552a71c18df761c
Alexandre Pelissier <alpeliss@w-r5-p4.s19.be>
2019-10-29 19:59:20 +0100

    exo 4 et 5

--- C01/main.c
+++ C01/main.c
@@ -6,7 +6,7 @@
 /*   By: alpeliss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 18:45:18 by alpeliss          #+#    #+#             */
-/*   Updated: 2019/10/29 19:14:40 by alpeliss         ###   ########.fr       */
+/*   Updated: 2019/10/29 19:57:52 by alpeliss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 void	ft_ft(int *nbr);
 void	ft_swap(int *a, int *b);
 void	ft_div_mod(int a, int b, int *div, int *mod);
+void	ft_ultimate_div_mod(int *a, int *b);
+void	ft_putstr(char *str);
 
 int main(void)
 {
@@ -45,5 +47,11 @@ int main(void)
 	b = 5;
 	ft_div_mod(a, b, div, mod);
 	printf("a = %d, b = %d, a / b = %d et a %% b = %d\n", a, b, *div, *mod);
+	*p = 45;
+	*q = 6;
+	printf("p = %d et q = %d -> ", *p, *q);
+	ft_ultimate_div_mod(p, q);
+	printf("p = %d et q = %d\n", *p, *q);
+	ft_putstr("Bonjour je suis un calamar\n");
 	return (0);
 }
