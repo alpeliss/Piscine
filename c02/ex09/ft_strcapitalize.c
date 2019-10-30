9c6b3969d893f3c1980759882a2159b9a2b2e986
Alexandre Pelissier <alpeliss@w-r4-p4.s19.be>
2019-10-30 16:23:17 +0100

    ex09

---
+++ c02/ex09/ft_strcapitalize.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alpeliss <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/30 16:05:25 by alpeliss          #+#    #+#             */
+/*   Updated: 2019/10/30 16:14:56 by alpeliss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		is_small(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
+
+int		is_al_num(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else if (c >= 'a' && c <= 'z')
+		return (1);
+	else if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+
+	if (!str)
+		return (str);
+	if (is_small(str[0]))
+		str[0] -= 32;
+	i = 1;
+	while (str[i])
+	{
+		if (is_small(str[i]) && !(is_al_num(str[i - 1])))
+			str[i] -= 32;
+		i++;
+	}	
+	return (str);
+}
--- c02/main.c
+++ c02/main.c
@@ -6,7 +6,7 @@
 /*   By: alpeliss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 14:20:00 by alpeliss          #+#    #+#             */
-/*   Updated: 2019/10/30 16:04:20 by alpeliss         ###   ########.fr       */
+/*   Updated: 2019/10/30 16:21:46 by alpeliss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,23 +23,26 @@ int		ft_str_is_uppercase(char *str);
 int		ft_str_is_printable(char *str);
 char	*ft_strupcase(char *str);
 char	*ft_strlowcase(char *str);
+char	*ft_strcapitalize(char *str);
 
 int		main(void)
 {
 	char	*dest;
 	char	*dest2;
 	char	*src;
+	char	*src2;
 
-	src = "Une courte phrase";
+	src2 = "Une courte phrase";
+	src = "42mots Cinquante+Et+Un Quarante-Deux;";
 	dest = (char *)malloc(30 * sizeof(char));
 	ft_strcpy(dest, src);
 	printf("-%s-\n", dest);
 	dest2 = (char *)malloc(30 * sizeof(char));
-	ft_strncpy(dest2, src, 4);
+	ft_strncpy(dest2, src2, 4);
 	printf("-%s-\n", dest2);	
-	ft_strncpy(dest2, src, 0);
+	ft_strncpy(dest2, src2, 0);
 	printf("-%s-\n", dest2);
-	ft_strncpy(dest2, src, 29);
+	ft_strncpy(dest2, src2, 29);
 	printf("-%s-\n", dest2);	
 	ft_strncpy(dest2, NULL, 29);
 	printf("-%s-\n", dest2);
@@ -53,7 +56,10 @@ int		main(void)
 	printf("%d%d\n",  ft_str_is_uppercase("!"),  ft_str_is_uppercase("v"));
 	printf("%d%d",  ft_str_is_printable(""),  ft_str_is_printable("134didqw$%^*9h;'{}|]["));
 	printf("%d\n", ft_str_is_printable("\n"));
-	printf("%s->", dest2);
-	printf("%s->",  ft_strupcase(dest2));
-	printf("%s\n",  ft_strlowcase(dest2));
+	printf("->%s\n", dest);
+	printf("->%s\n",  ft_strupcase(dest));
+	printf("->%s\n",  ft_strlowcase(dest));
+	ft_strupcase("");
+	ft_strlowcase("");
+	printf("->%s", ft_strcapitalize(dest));
 }
