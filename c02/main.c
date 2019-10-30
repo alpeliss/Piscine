0073a6a96f3a483c590834fc27480db6c3400bcd
Alexandre Pelissier <alpeliss@w-r4-p4.s19.be>
2019-10-30 15:13:27 +0100

    ex03

--- c02/main.c
+++ c02/main.c
@@ -6,7 +6,7 @@
 /*   By: alpeliss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/30 14:20:00 by alpeliss          #+#    #+#             */
-/*   Updated: 2019/10/30 15:07:51 by alpeliss         ###   ########.fr       */
+/*   Updated: 2019/10/30 15:13:01 by alpeliss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 char	*ft_strcpy(char *dest, char *src);
 char	*ft_strncpy(char *dest, char *src, unsigned int n);
 int		ft_str_is_alpha(char *str);
+int		ft_str_is_numeric(char *str);
 
 int		main(void)
 {
@@ -38,5 +39,8 @@ int		main(void)
 	ft_strncpy(dest2, NULL, 29);
 	printf("-%s-\n", dest2);
 	printf("%d", ft_str_is_alpha("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"));
+	printf("%d", ft_str_is_alpha(""));
 	printf("%d%d%d\n", ft_str_is_alpha("!"), ft_str_is_alpha("["), ft_str_is_alpha("~"));
+	printf("%d%d", ft_str_is_numeric(""), ft_str_is_numeric("0123456789"));
+	printf("%d%d\n", ft_str_is_numeric("+"), ft_str_is_numeric(";"));
 }
