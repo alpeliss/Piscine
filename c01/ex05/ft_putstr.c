afc5b2dc11c73889694e7622daf1c4087857acdc
Alexandre Pelissier <alpeliss@w-r4-p4.s19.be>
2019-10-31 17:37:55 +0100

    secur ft_putstr

--- c01/ex05/ft_putstr.c
+++ c01/ex05/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: alpeliss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/29 19:56:45 by alpeliss          #+#    #+#             */
-/*   Updated: 2019/10/31 17:29:54 by alpeliss         ###   ########.fr       */
+/*   Updated: 2019/10/31 17:37:35 by alpeliss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ void	ft_putstr(char *str)
 	int	i;
 
 	i = 0;
-	while (str[i])
-		i++;
-	write(1, str, i);
+	if (str)
+	{
+		while (str[i])
+			i++;
+		write(1, str, i);
+	}
 }
