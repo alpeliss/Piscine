7a3bfd6904f94597336915fac80b4f2d42a655eb
Alexandre Pelissier <alpeliss@w-r3-p4.s19.be>
2019-11-14 20:14:23 +0100

    ex01

--- c11/ex01/ft_map.c
+++ c11/ex01/ft_map.c
@@ -1,47 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_map.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alpeliss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/14 19:22:22 by alpeliss          #+#    #+#             */
-/*   Updated: 2019/11/14 20:13:13 by alpeliss         ###   ########.fr       */
+/*   Created: 2019/11/14 20:08:32 by alpeliss          #+#    #+#             */
+/*   Updated: 2019/11/14 20:13:49 by alpeliss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "unistd.h"
+#include "stdlib.h"
 
-void ft_foreach(int *tab, int length, void(*f)(int));
-int *ft_map(int *tab, int length, int(*f)(int));
-
-void	ft_putnbr(int a)
+int	*ft_map(int *tab, int length, int (*f)(int))
 {
-	char c;
-
-	c = '0' + a;
-	write(1, &c, 1);
-}
-
-int		plus_one(int i)
-{
-	return (i + 1);
-}
-
-int	main(void)
-{
-	int	tab[10];
 	int	i;
-	int	*n_tab;
+	int	*new_tab;
 
+	new_tab = (int *)malloc(length * sizeof(int));
 	i = 0;
-	while (i < 10)
+	while (i < length)
 	{
-		tab[i] = i;
+		new_tab[i] = f(tab[i]);
 		i++;
 	}
-	ft_foreach(tab, 10, &ft_putnbr);
-	n_tab = ft_map(tab, 10, &plus_one);
-	write(1, "\n", 1);
-	ft_foreach(n_tab, 10, &ft_putnbr);
+	return (new_tab);
 }
