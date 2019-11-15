5e4143b7ce56e92a938706bba824d366786baf1e
Alexandre Pelissier <alpeliss@w-r3-p5.s19.be>
2019-11-15 14:11:39 +0100

    little norm

--- c11/ex06/ft_sort_string_tab.c
+++ c11/ex06/ft_sort_string_tab.c
@@ -1,26 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   do_op.h                                            :+:      :+:    :+:   */
+/*   ft_sort_string_tab.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alpeliss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/15 14:10:22 by alpeliss          #+#    #+#             */
-/*   Updated: 2019/11/15 14:11:05 by alpeliss         ###   ########.fr       */
+/*   Created: 2019/11/15 14:04:02 by alpeliss          #+#    #+#             */
+/*   Updated: 2019/11/15 14:10:07 by alpeliss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef DO_OP_H
-# define DO_OP_H
+static int
 
-# include "unistd.h"
+void ft_sort_string_tab(char **tab)
+{
 
-long	ft_atol(char *str);
-void	ft_putnbr(long nb);
-void	p_add(long a, long b);
-void	p_sub(long a, long b);
-void	p_mul(long a, long b);
-void	p_div(long a, long b);
-void	p_mod(long a, long b);
-
-#endif
+}
